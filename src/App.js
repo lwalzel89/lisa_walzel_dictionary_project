bddb1a72e776cf428cbe4a689d2653eeaaee6b6a
lwalzel <lisa.walzel@hotmail.com>
2024-12-29 19:40:53 +0100

    Setting up search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./shecodes.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
